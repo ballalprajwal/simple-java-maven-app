ce238a41234613739114472eb8c7ab500ac52603
ballalprajwal <106139161+ballalprajwal@users.noreply.github.com>
2022-06-01 11:33:11 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,7 +34,7 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello India" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello india"; + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello World!\"");
         }
